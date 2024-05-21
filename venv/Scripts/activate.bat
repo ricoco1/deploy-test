8d5fa9aafb1e04ccda4a020d3133e341652ac626
Raffli-Dev <raflibismaaryasatya@gmail.com>
2024-05-21 22:07:01 +0700

    first commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\LENOVO\Documents\MSIB-Prak\MSIB_Akhir\venv
+set VIRTUAL_ENV=C:\Users\LENOVO\Documents\MSIB-Prak\MSIB_Akhir\MSIB-Reservasi-Hotel\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
