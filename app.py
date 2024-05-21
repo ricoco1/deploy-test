eced39e2cdcc98bcd3b548afe304465d2dc7f199
ricoco1 <ricopratama3009@gmail.com>
2024-05-21 21:51:52 +0700

    Menambahkan halaman room dan memperbaiki struktur folder

--- app.py
+++ app.py
@@ -22,35 +22,43 @@ db = client[DB_NAME]
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return render_template('home/index.html')
 
 @app.route('/rooms', methods=['GET'])
 def rooms():
-    return render_template('rooms.html')
+    return render_template('room/rooms.html')
+
+@app.route('/rooms/deluxe', methods=['GET'])
+def deluxe_room():
+    return render_template('room/rooms.html')
+
+@app.route('/rooms/deluxe_family', methods=['GET'])
+def deluxe_family():
+    return render_template('room/deluxe_family.html')
 
 @app.route('/facilities', methods=['GET'])
 def facilities():
-    return render_template('facilities.html')
+    return render_template('facilities/facilities.html')
 
 @app.route('/gallery', methods=['GET'])
 def gallery():
-    return render_template('gallery.html')
+    return render_template('gallery/gallery.html')
 
 @app.route('/contact', methods=['GET'])
 def contact():
-    return render_template('contact.html')
+    return render_template('contact/contact.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    return render_template('login/login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    return render_template('register/register.html')
 
 @app.route('/book', methods=['GET', 'POST'])
 def book():
-    return render_template('book.html')
+    return render_template('book/book.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
