b5558d090e5d867be6ecb305eeddeea70ee3670c
ricoco1 <ricopratama3009@gmail.com>
2024-06-01 08:09:26 +0700

    Menambahkan halaman Admin dan logic admin

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from dotenv import load_dotenv
 import hashlib
 from datetime import datetime, timedelta
 from werkzeug.utils import secure_filename
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = 'static/img/uploads/profile'
-app.config['MAX_CONTENT_PATH'] = 16 * 1024 * 1024  # Max Upload 16mb
+app.config['MAX_CONTENT_PATH'] = 1 * 1024 * 1024  # Max Upload 16mb
 app.secret_key = 'supersecretkey'
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -28,6 +29,13 @@ def get_user_info():
         email = session['email']
         user_info = db.users.find_one({'email': email})
         return user_info
+    return None
+
+def get_admin_info():
+    if 'logged_in' in session:
+        email = session['email']
+        admin_info = db.admin.find_one({'email': email})
+        return admin_info
     return None
 
 @app.route('/', methods=['GET'])
@@ -58,6 +66,10 @@ def gallery():
 def contact():
     return render_template('contact/contact.html')
 
+@app.route('/book', methods=['GET'])
+def book():
+    return render_template('book/book.html')
+
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
     email = request.form.get('email')
@@ -73,10 +85,6 @@ def sign_in():
             session['full_name'] = user['full_name']
             return jsonify({'result': 'success', 'msg': 'Login berhasil'})
     return jsonify({'result': 'fail', 'msg': 'Password Salah!!!'})
-
-@app.route('/book', methods=['GET'])
-def book():
-    return render_template('book/book.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -272,6 +280,258 @@ def user_book():
 def logout():
     session.clear()
     return redirect(url_for('home'))
+
+# Route admin
+@app.route("/admin/login")
+def login_admin():
+    msg = request.args.get("msg")
+    return render_template("admin/login/login.html", msg=msg)
+
+@app.route('/admin/sign_in', methods=['POST'])
+def admin_login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    admin = db.admin.find_one({'email': email})
+
+    if admin:
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        if admin['password'] == hashed_password:
+            session['logged_in'] = True
+            session['email'] = email
+            session['full_name'] = admin['full_name']
+            return jsonify({'result': 'success', 'msg': 'Login berhasil'})
+    return jsonify({'result': 'fail', 'msg': 'Password Salah!!!'})
+
+@app.route('/admin/register', methods=['GET', 'POST'])
+def admin_register():
+    if request.method == 'POST':
+        full_name = request.form.get('fullName')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirmPassword')
+        role = request.form.get('role')
+
+        if not full_name or not email or not password or not confirm_password:
+            return jsonify({'error': 'Semua field yang diberi tanda * harus diisi.'}), 400
+
+        if password != confirm_password:
+            return jsonify({'error': 'Kata sandi dan konfirmasi kata sandi tidak cocok.'}), 400
+
+        if db.users.find_one({'email': email}):
+            return jsonify({'error': 'Email sudah digunakan.'}), 400
+
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        profile_picture_url = url_for('static', filename='img/uploads/profile/profile_placeholder.png')
+
+        user_data = {
+            'full_name': full_name,
+            'email': email,
+            'password': hashed_password,
+            'role': role,
+            'profile_picture_url': profile_picture_url
+            
+        }
+        db.admin.insert_one(user_data)
+        return jsonify({'success': 'Pendaftaran berhasil!'}), 200
+
+    return render_template('admin/register/register.html')
+
+@app.route('/admin/logout')
+def admin_logout():
+    session.pop('admin_logged_in', None)
+    session.pop('admin_email', None)
+    session.pop('admin_full_name', None)
+    return redirect(url_for('login_admin'))
+
+@app.route('/admin/dashboard')
+def admin_dashboard():
+    admin_info = get_admin_info()
+    if admin_info:
+        return render_template('admin/dashboard/index.html', admin_info=admin_info)
+    else:
+        return redirect(url_for('login_admin'))
+
+@app.route('/admin/profile', methods=['GET'])
+def admin_profile():
+    admin_info = get_admin_info()
+    if admin_info:
+        return render_template('admin/profile/profile.html', admin_info=admin_info)
+    else:
+        return redirect(url_for('login_admin'))
+
+@app.route('/admin/profile/edit', methods=['GET', 'POST'])
+def admin_edit_profile():
+    if 'logged_in' in session:
+        email = session['email']
+        admin_info = db.admin.find_one({'email': email})
+
+        if request.method == 'POST':
+            full_name = request.form.get('fullName')
+            phone_number = request.form.get('phoneNumber')
+            birth_date = request.form.get('birthDate')
+            gender = request.form.get('gender')
+            address = request.form.get('address')
+            city = request.form.get('city')
+            region = request.form.get('region')
+            postal_code = request.form.get('postalCode')
+
+            db.admin.update_one(
+                {'email': email},
+                {'$set': {
+                    'full_name': full_name,
+                    'phone_number': phone_number,
+                    'birth_date': birth_date,
+                    'gender': gender,
+                    'address': address,
+                    'city': city,
+                    'region': region,
+                    'postal_code': postal_code
+                }}
+            )
+            return redirect(url_for('admin_profile'))
+        
+        return render_template('admin/profile/edit_profile.html', admin_info=admin_info)
+    else:
+        return redirect(url_for('admin_login'))
+
+@app.route('/admin/profile/upload_photo', methods=['POST'])
+def admin_upload_photo():
+    if 'logged_in' in session:
+        email = session['email']
+        if 'profile_picture' in request.files:
+            profile_picture = request.files['profile_picture']
+            if profile_picture:
+                filename = secure_filename(profile_picture.filename)
+                profile_picture_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                profile_picture.save(profile_picture_path)
+                
+                profile_picture_url = url_for('static', filename='img/uploads/profile/' + filename)
+                db.admin.update_one(
+                    {'email': email},
+                    {'$set': {'profile_picture_url': profile_picture_url}}
+                )
+        return redirect(url_for('admin_profile'))
+    else:
+        return redirect(url_for('admin_login'))
+
+@app.route('/admin/user', methods=['GET'])
+def admin_user():
+    admin_info = get_admin_info()
+    if admin_info:
+        search_query = request.args.get('search')
+        per_page = 5
+        page = int(request.args.get('page', 1))
+        
+        if search_query:
+            users = list(db.users.find({'email': {'$regex': search_query, '$options': 'i'}}).skip((page - 1) * per_page).limit(per_page))
+            total_users = db.users.count_documents({'email': {'$regex': search_query, '$options': 'i'}})
+        else:
+            users = list(db.users.find().skip((page - 1) * per_page).limit(per_page))
+            total_users = db.users.count_documents({})
+        
+        total_pages = (total_users + per_page - 1) // per_page
+        return render_template('admin/user/user.html', users=users, admin_info=admin_info, page=page, total_pages=total_pages, total_users=total_users)
+    else:
+        return redirect(url_for('login_admin'))
+
+@app.route('/admin/delete_user', methods=['POST'])
+def admin_delete_user():
+    user_id = request.form.get('user_id')
+    if user_id:
+        result = db.users.delete_one({'_id': ObjectId(user_id)})
+        if result.deleted_count > 0:
+            return jsonify({'result': 'success'})
+        else:
+            return jsonify({'result': 'error', 'msg': 'Gagal menghapus pengguna.'})
+    return jsonify({'result': 'error', 'msg': 'ID pengguna tidak valid.'})
+
+@app.route('/admin/bulk_delete_users', methods=['POST'])
+def admin_bulk_delete_users():
+    user_ids = request.form.getlist('user_ids[]')
+    if user_ids:
+        object_ids = [ObjectId(user_id) for user_id in user_ids]
+        result = db.users.delete_many({'_id': {'$in': object_ids}})
+        if result.deleted_count > 0:
+            return jsonify({'result': 'success'})
+        else:
+            return jsonify({'result': 'error', 'msg': 'Gagal menghapus pengguna.'})
+    return jsonify({'result': 'error', 'msg': 'Tidak ada ID pengguna yang valid.'})
+
+@app.route('/admin/user/profile/<user_id>', methods=['GET', 'POST'])
+def admin_edit_user_profile(user_id):
+    if 'logged_in' in session:
+        admin_info = get_admin_info()
+        if admin_info:
+            user_info = db.users.find_one({'_id': ObjectId(user_id)})
+
+            if request.method == 'POST':
+                full_name = request.form.get('fullName')
+                phone_number = request.form.get('phoneNumber')
+                birth_date = request.form.get('birthDate')
+                gender = request.form.get('gender')
+                address = request.form.get('address')
+                city = request.form.get('city')
+                region = request.form.get('region')
+                postal_code = request.form.get('postalCode')
+
+                db.users.update_one(
+                    {'_id': ObjectId(user_id)},
+                    {'$set': {
+                        'full_name': full_name,
+                        'phone_number': phone_number,
+                        'birth_date': birth_date,
+                        'gender': gender,
+                        'address': address,
+                        'city': city,
+                        'region': region,
+                        'postal_code': postal_code
+                    }}
+                )
+                return redirect(url_for('admin_edit_user_profile', user_id=user_id))
+
+            return render_template('admin/user/edit_user_profile.html', admin_info=admin_info, user_info=user_info)
+        else:
+            return redirect('/admin/login')
+    else:
+        return redirect('/admin/login')
+
+@app.route('/admin/user/profile/<user_id>/upload_photo', methods=['POST'])
+def admin_upload_user_photo(user_id):
+    if 'logged_in' in session:
+        admin_info = get_admin_info()
+        if admin_info and 'profile_picture' in request.files:
+            profile_picture = request.files['profile_picture']
+            if profile_picture:
+                filename = secure_filename(profile_picture.filename)
+                profile_picture_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                profile_picture.save(profile_picture_path)
+                
+                profile_picture_url = url_for('static', filename='img/uploads/profile/' + filename)
+                db.users.update_one(
+                    {'_id': ObjectId(user_id)},
+                    {'$set': {'profile_picture_url': profile_picture_url}}
+                )
+        return redirect(url_for('admin_edit_user_profile', user_id=user_id))
+    else:
+        return redirect(url_for('login_admin'))
+
+
+@app.route('/admin/rooms', methods=['GET'])
+def admin_room():
+    admin_info = get_admin_info()
+    if admin_info:
+        return render_template('admin/room/room.html', admin_info=admin_info)
+    else:
+        return redirect(url_for('login_admin'))
+
+@app.route('/admin/guest', methods=['GET'])
+def admin_guest():
+    admin_info = get_admin_info()
+    if admin_info:
+        return render_template('admin/guest/guest.html', admin_info=admin_info)
+    else:
+        return redirect(url_for('login_admin'))
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port=5000, debug=True)
